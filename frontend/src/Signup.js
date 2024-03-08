c42741b85c6b7683ddb51ffdf74f6a21b8383bcd
RubenNhez <ruben.nhez@gmail.com>
2024-03-08 00:12:10 +0000

    add login and register form

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function Login() {
+function Signup() {
     return (
         <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
             <div className='bg-white p-3 rounded w-25'>
-                <h2>Sign In</h2>
+                <h2>Sign Up</h2>
                 <from action = "">
+                <div className='mb-3'>
+                        <label htmlFor="name"><strong>Name</strong></label>
+                        <input type="text" placeholder='Enter Name' className='form-control rounded-0'/>
+                    </div>
                     <div className='mb-3'>
                         <label htmlFor="email"><strong>Email</strong></label>
                         <input type="email" placeholder='Enter Email' className='form-control rounded-0'/>
@@ -15,13 +19,13 @@ function Login() {
                         <label htmlFor="password"><strong>Password</strong></label>
                         <input type="password" placeholder='Enter Password' className='form-control rounded-0' />
                     </div>
-                    <button className='btn btn-success w-100 rounded-0'><strong> Login </strong></button>
+                    <button className='btn btn-success w-100 rounded-0'><strong> Sign up </strong></button>
 
-                    <Link to="/signup" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Create Account</Link>
+                    <Link to="/" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Login</Link>
                 </from>
             </div>
         </div>
     )
 }
 
-export default Login
+export default Signup
